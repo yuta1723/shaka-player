49d08b22950778765e028932e02f74e1ff31681f
yuta naito <yuta.naito@play.jp>
2019-12-27 00:53:30 +0900

    ServiceCertificateを追加.

--- myapp.js
+++ myapp.js
@@ -3,6 +3,8 @@
 var manifestUri =
     'https://storage.googleapis.com/shaka-demo-assets/angel-one-clearkey/dash.mpd';
 
+// var manifestUri = 'https://path/content.mpd';
+
 function initApp() {
   // Install built-in polyfills to patch browser incompatibilities.
   shaka.polyfill.installAll();
@@ -31,14 +33,30 @@ function initPlayer() {
   // }
   // player.configure('drm.servers.org\.w3\.clearkey','https://cwip-shaka-proxy.appspot.com/clearkey?_u3wDe7erb7v8Lqt8A3QDQ=ABEiM0RVZneImaq7zN3u_w');
 
+  var widevineServiceCertificate = 'CsECCAMSEBcFuRfMEgSGiwYzOi93KowYgrSCkgUijgIwggEKAoIBAQCZ7Vs7Mn2rXiTvw7YqlbWYUgrVvMs3UD4GRbgU2Ha430BRBEGtjOOtsRu4jE5yWl5KngeVKR1YWEAjp+GvDjipEnk5MAhhC28VjIeMfiG/+/7qd+EBnh5XgeikX0YmPRTmDoBYqGB63OBPrIRXsTeo1nzN6zNwXZg6IftO7L1KEMpHSQykfqpdQ4IY3brxyt4zkvE9b/tkQv0x4b9AsMYE0cS6TJUgpL+X7r1gkpr87vVbuvVk4tDnbNfFXHOggrmWEguDWe3OJHBwgmgNb2fG2CxKxfMTRJCnTuw3r0svAQxZ6ChD4lgvC2ufXbD8Xm7fZPvTCLRxG88SUAGcn1oJAgMBAAE6FGxpY2Vuc2Uud2lkZXZpbmUuY29tEoADrjRzFLWoNSl/JxOI+3u4y1J30kmCPN3R2jC5MzlRHrPMveoEuUS5J8EhNG79verJ1BORfm7BdqEEOEYKUDvBlSubpOTOD8S/wgqYCKqvS/zRnB3PzfV0zKwo0bQQQWz53ogEMBy9szTK/NDUCXhCOmQuVGE98K/PlspKkknYVeQrOnA+8XZ/apvTbWv4K+drvwy6T95Z0qvMdv62Qke4XEMfvKUiZrYZ/DaXlUP8qcu9u/r6DhpV51Wjx7zmVflkb1gquc9wqgi5efhn9joLK3/bNixbxOzVVdhbyqnFk8ODyFfUnaq3fkC3hR3f0kmYgI41sljnXXjqwMoW9wRzBMINk+3k6P8cbxfmJD4/Paj8FwmHDsRfuoI6Jj8M76H3CTsZCZKDJjM3BQQ6Kb2m+bQ0LMjfVDyxoRgvfF//M/EEkPrKWyU2C3YBXpxaBquO4C8A0ujVmGEEqsxN1HX9lu6c5OMm8huDxwWFd7OHMs3avGpr7RP7DUnTikXrh6X0';
+
+
+
   player.configure({
     drm: {
       servers: {
-        'org.w3.clearkey': 'https://cwip-shaka-proxy.appspot.com/clearkey?_u3wDe7erb7v8Lqt8A3QDQ=ABEiM0RVZneImaq7zN3u_w'
+        // Android/iOS は wvstreamidだが、HTML5は streamid .
+        // PSSH はdash manifest に記載されている
+        // またURL encode を行わないと、 php の POSTの際に pssh に + が入っていたら空白になってしまい問題になる。
+        'com.widevine.alpha': 'https://tsg01.uliza.jp/ulizahtml5/dash_api/rights_issuer.php?streamid=bbb_2M_100sec&pssh=AAAAWHBzc2gAAAAA7e%2BLqXnWSs6jyCfc1R0h7QAAADgIARIQxXcyfkNpsb3dz3hYVXtRrxoMc2tpbGx1cHZpZGVvIg1iYmJfMk1fMTAwc2VjKgVTRF9IRA%3D%3D'
+      },
+      advanced: {
+        'com.widevine.alpha': {
+          'serverCertificate': base64ToUint8Array(widevineServiceCertificate),
+          'videoRobustness': 'SW_SECURE_CRYPTO',
+          'audioRobustness': 'SW_SECURE_CRYPTO'
+        }
       }
     }
   });
 
+  // https://shaka-player-demo.appspot.com/docs/api/tutorial-license-wrapping.html
+  // 再生できないヒントはここにあるかも。
 
   // Attach player to the window to make it easy to access in the JS console.
   window.player = player;
@@ -63,5 +81,16 @@ function onError(error) {
   // Log the error.
   console.error('Error code', error.code, 'object', error);
 }
+
+function base64ToUint8Array(base64) {
+    var binary_string =  window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes
+}
+
 
 document.addEventListener('DOMContentLoaded', initApp);
